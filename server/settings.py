ffd35734b30b2edfaecf1c1b9e2a138fc020b4e3
Ashraf Eldawody <ashraf6450@gmail.com>
2022-04-23 10:27:44 +0200

    Adding Dependancies + DB Connection

--- server/settings.py
+++ server/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fund-app',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
