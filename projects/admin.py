452ffcb3108c43558c38b6d5eb83e0423fe9d579
amraabdallah <amr.abdalaziz.abdallah@gmail.com>
2022-04-24 01:38:57 +0200

    register all the models to admin panel

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from categories.models import Category
+from comments.models import Comment
+from users.models import User
+from tags.models import Tag
+from donations.models import Donation
+
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Donation)
+
+
