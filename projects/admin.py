467a611299f4ac31d296458c779081fe6b25e578
amraabdallah <amr.abdalaziz.abdallah@gmail.com>
2022-04-24 02:04:09 +0200

    register the rest of the models

--- projects/admin.py
+++ projects/admin.py
@@ -7,11 +7,13 @@ from comments.models import Comment
 from users.models import User
 from tags.models import Tag
 from donations.models import Donation
+from .models import Project, Image
 
 admin.site.register(Category)
 admin.site.register(Comment)
 admin.site.register(User)
 admin.site.register(Tag)
 admin.site.register(Donation)
-
+admin.site.register(Project)
+admin.site.register(Image)
 
