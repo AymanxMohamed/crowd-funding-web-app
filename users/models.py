c3f2f5b7b0d71f45dfb07fc2dfd2dfa05e37d722
Ayman Kheireldeen <aymanmido067@gmail.com>
2022-04-25 12:41:03 +0200

    Modifying user model self method and creating user serializer

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
     first_name = models.CharField(max_length=255)
@@ -12,4 +13,4 @@ class User(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.last_name}"
