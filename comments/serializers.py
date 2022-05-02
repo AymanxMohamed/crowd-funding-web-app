c97c991c1a5f806ad90fea62a2986a2f1a68c331
amraabdallah <amr.abdalaziz.abdallah@gmail.com>
2022-05-02 23:55:34 +0200

    add related project field to comment api

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 
 from comments.models import Comment
-
+from projects.models import Project
 
 class CommentSerializer(serializers.ModelSerializer):
+    project = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all())
     class Meta:
         model = Comment
-        fields = ['id', 'content', 'created_at', 'updated_at', 'owner']
+        fields = "__all__"
         # read_only_fields = ('created_at', 'updated_at')
         # depth = 1
