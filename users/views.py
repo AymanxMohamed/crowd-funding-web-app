f41b51be16f17d54196889a67f5a5ad4de712e46
Ayman Kheireldeen <aymanmido067@gmail.com>
2022-05-03 12:32:07 +0200

    adding the send mail functionlaity after the registeration

--- users/views.py
+++ users/views.py
@@ -6,10 +6,18 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.core.mail import send_mail
 
+from django.utils.encoding import force_bytes, force_str, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.http import HttpResponsePermanentRedirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+
+from .models import User
 from .serializers import UserSerializer
 
-EMAIL_SUBJECT = 'User Account Activation ❤️'
-EMAIL_MESSAGE = 'Please activate your account via this link 🤌'
+from .utils import token_generator
+
+BASE_FRONT_URL = 'http/localhost:3000'
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -41,13 +49,17 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 @api_view(['POST'])
 def register(request):
+    email_subject = 'User Account Activation ❤️'
     user_serializer = UserSerializer(data=request.data)
     if user_serializer.is_valid():
-        # user_serializer.save()
+        user = user_serializer.save()
+        print(user.id)
         print(user_serializer.data["email"])
+        activate_url = mail_activation_link(request, user)
+        email_body = f"Hi {user.first_name} {user.last_name} \n Please Use this link to verify your account \n {activate_url}"
         send_mail(
-            subject=EMAIL_SUBJECT,
-            message=EMAIL_MESSAGE,
+            subject=email_subject,
+            message=email_body,
             from_email='noreply@crowd.com',
             recipient_list=[user_serializer.data["email"]],
         )
@@ -55,3 +67,25 @@ def register(request):
     return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+def mail_activation_link(request, user):
+    uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+    domain = get_current_site(request).domain
+    link = reverse('activate', kwargs={
+        'uidb64': uidb64,
+        'token': token_generator.make_token(user)
+    })
+    return f'http://{domain}{link}'
+
+
+@api_view(['GET'])
+def activate(request, uidb64, token):
+    id = force_str(urlsafe_base64_decode(uidb64))
+    user = User.objects.get(pk=id)
+
+    if not token_generator.check_token(user, token) or user.is_active:
+        return HttpResponsePermanentRedirect(f'{BASE_FRONT_URL}/auth/login?message="Account Already Activated"')
+
+    user.is_active = True
+    user.save()
+
+    return HttpResponsePermanentRedirect(f'{BASE_FRONT_URL}/auth/login?message="Account Activated Successfully"')
