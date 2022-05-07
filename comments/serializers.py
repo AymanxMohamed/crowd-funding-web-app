ddac37e4bdff7e192f3e716bbd344d8165e89c80
amraabdallah <amr.abdalaziz.abdallah@gmail.com>
2022-05-08 01:36:35 +0200

    modifying comment serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -2,11 +2,13 @@ from rest_framework import serializers
 
 from comments.models import Comment
 from projects.models import Project
+from users.serializers import UserSerializer
 
 class CommentSerializer(serializers.ModelSerializer):
     project = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all())
+    owner = UserSerializer(many=False, read_only=True)
+    
     class Meta:
         model = Comment
         fields = "__all__"
-        # read_only_fields = ('created_at', 'updated_at')
-        # depth = 1
+
--- ratings/urls.py
+++ ratings/urls.py
@@ -4,4 +4,8 @@ from ratings import views
 
 urlpatterns = [
     path('', views.api_ratings_list, name='rating_list'),
+    path('create/', views.api_create_rating, name='create_rating'),
+    path('<int:pk>/', views.api_get_rating_by_id, name='get_rating_by_id'),
+    path('<int:pk>/update/', views.api_update_rating, name='update_rating'),
+    path('<int:pk>/delete/', views.api_delete_rating, name='delete_rating'),
 ]
