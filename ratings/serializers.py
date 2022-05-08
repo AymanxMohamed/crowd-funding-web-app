0375bf279135ca69b9be87f6a86479a9b6ea963c
amraabdallah <amr.abdalaziz.abdallah@gmail.com>
2022-05-08 04:04:02 +0200

    rating crud *POST has a bug*

--- ratings/serializers.py
+++ ratings/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from ratings.models import Rating
 
-
+from users.serializers import UserSerializer
+from projects.serializers import ProjectSerializer
 class RatingSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True, required=False, allow_null=True)
+    project = serializers.StringRelatedField(read_only=True, required=False, allow_null=True)
     class Meta:
         model = Rating
         fields = '__all__'
--- ratings/views.py
+++ ratings/views.py
@@ -1,23 +1,47 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+import ratings
+
+from ratings.serializers import RatingSerializer
+from ratings.models import Rating
 
 @api_view(['GET'])
 def api_ratings_list(request):
-    pass
+    ratings = Rating.objects.all()
+    serialized_ratings = RatingSerializer(ratings, many=True)
+    return Response(serialized_ratings.data)
 
 @api_view(['POST'])
 def api_create_rating(request):
-    pass
+    print("##############BUG##HERE###############################", request.data)
+    serialized_rating = RatingSerializer(data=request.data)
+    print("##############BUG##HERE###############################", serialized_rating)
+    if serialized_rating.is_valid():
+        serialized_rating.save()
+        return Response(serialized_rating.data, status=status.HTTP_201_CREATED)
+    return Response(serialized_rating.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
-def api_get_rating_by_id(request):
-    pass
-
+def api_get_rating_by_id(request, pk):
+    rating = get_object_or_404(Rating, id=pk)
+    serialized_rating = RatingSerializer(rating)
+    return Response(serialized_rating.data, status=status.HTTP_200_OK)
+    
+    
 @api_view(['PUT'])
-def api_update_rating(request):
-    pass
-
+def api_update_rating(request, pk):
+    rating = get_object_or_404(Rating, id=pk)
+    serialized_rating = RatingSerializer(instance=rating, data=request.data)
+    if serialized_rating.is_valid():
+        serialized_rating.save()
+        return Response(serialized_rating.data, status=status.HTTP_200_OK)
+    return Response(serialized_rating.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 @api_view(['DELETE'])
-def api_delete_rating(request):
-    pass
+def api_delete_rating(request, pk):
+    rating = get_object_or_404(Rating, id=pk)
+    rating.delete()
+    return Response('success',status=status.HTTP_200_OK)
+    
